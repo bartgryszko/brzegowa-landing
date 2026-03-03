eb849d84d492d038c6c391f340b3687e01c34afe
Bart Gryszko <b@gryszko.com>
2026-03-03 18:28:27 +0100

    Remove CAPI test_event_code, go live

--- api/contact.js
+++ api/contact.js
@@ -44,8 +44,6 @@ async function sendCAPI({ name, phone, eventId, fbc, fbp, userAgent, ip, sourceU
       event_source_url: sourceUrl || 'https://jakdlasiebie.com',
       user_data: userData,
     }],
-    // TODO: usunąć po testach
-    test_event_code: 'TEST65235',
   };
 
   try {
